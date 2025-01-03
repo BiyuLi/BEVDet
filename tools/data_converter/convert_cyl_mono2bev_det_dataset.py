8dc66b5019ecef71bdb5881d5988653fb2103399
Biyu Li <biyu.li@hongjingdrive.com>
2025-01-03 15:26:43 +0800

    update dataset config
    update

--- tools/data_converter/convert_cyl_mono2bev_det_dataset.py
+++ tools/data_converter/convert_cyl_mono2bev_det_dataset.py
@@ -2,25 +2,73 @@ import pickle
 import json
 import numpy as np
 from pyquaternion import Quaternion
+import argparse
 
-vehicle_classes = ['sedan', 'suv', 'bus', 'tiny_car', 'van', 'lorry', 'truck',
-                   'engineering_vehicle', 'tanker', 'cementing_unit', 'semi-trailer',
-                   'fire_trucks', 'ambulances', 'police_vehicle',
-                   'bicycle', 'motor', 'fork_truck', 'travel_trailer']
-person_classes = ['pedestrian']
-rider_classes = ['rider']
-rear_classes = ['rear', 'head', 'wheels']
-static_obs_classes = ['animal', 'stacked_trolleys',
-                      'non-motor_vehicle_group', 'crowd', 'cement_column', 'bollard',
-                      'folding_warning_sign', 'traffic_cone', 'parking_lock_open',
-                      'parking_lock_close', 'fire_hydrant_air', 'fire_hydrant_gnd',
-                      'cement_sphere', 'water_barrier', 'parking_lot_gate_open', 'parking_lot_gate_close', 'trolley',
-                      'charging_pile', 'reflector', 'distribution_box', 'single_fence', 'multi_fence', 'trash_can',
-                      'parking_sign', 'triangle_warning',
-                      'crash_barrel',
-                      ]
-ignore_classes = ['nonmotor_vehicle_group', 'fire_hydrant', 'undefined_vehicle', 'undefine_vehicle']
-class_names = vehicle_classes + person_classes + rider_classes
+class_names = {'sedan': 'vehicle',
+               'suv': 'vehicle',
+               'bus': 'vehicle',
+               'tiny_car': 'vehicle',
+               'van': 'vehicle',
+               'lorry': 'vehicle',
+               'truck': 'vehicle',
+               'engineering_vehicle': 'vehicle',
+               'tanker': 'vehicle',
+               'cementing_unit': 'vehicle',
+               'semi-trailer': 'vehicle',
+               'fire_trucks': 'vehicle',
+               'ambulances': 'vehicle',
+               'police_vehicle': 'vehicle',
+               'bicycle': 'vehicle',
+               'motor': 'vehicle',
+               'fork_truck': 'vehicle',
+               'travel_trailer': 'vehicle',
+               'pedestrian': 'pedestrian',
+               'rider': 'rider',
+               # 'animal': 'sod',
+               # 'stacked_trolleys': 'sod',
+               # 'non-motor_vehicle_group': 'sod',
+               # 'crowd': 'sod',
+               # 'cement_column': 'sod',
+               # 'bollard': 'sod',
+               # 'folding_warning_sign': 'sod',
+               # 'traffic_cone': 'sod',
+               # 'parking_lock_open': 'sod',
+               # 'parking_lock_close': 'sod',
+               # 'fire_hydrant_air': 'sod',
+               # 'fire_hydrant_gnd': 'sod',
+               # 'cement_sphere': 'sod',
+               # 'water_barrier': 'sod',
+               # 'parking_lot_gate_open': 'sod',
+               # 'parking_lot_gate_close': 'sod',
+               # 'trolley': 'sod',
+               # 'charging_pile': 'sod',
+               # 'reflector': 'sod',
+               # 'distribution_box': 'sod',
+               # 'single_fence': 'sod',
+               # 'multi_fence': 'sod',
+               # 'trash_can': 'sod',
+               # 'parking_sign': 'sod',
+               # 'triangle_warning': 'sod',
+               # 'crash_barrel': 'sod',
+               }
+branch_mapping = {'vehicle': 0,
+                  'pedestrian': 1,
+                  'rider': 2,
+                  # 'sod': 3
+                  }
+# person_classes = ['pedestrian']
+# rider_classes = ['rider']
+# sod_classes = ['animal', 'stacked_trolleys',
+#                'non-motor_vehicle_group', 'crowd', 'cement_column', 'bollard',
+#                'folding_warning_sign', 'traffic_cone', 'parking_lock_open',
+#                'parking_lock_close', 'fire_hydrant_air', 'fire_hydrant_gnd',
+#                'cement_sphere', 'water_barrier', 'parking_lot_gate_open', 'parking_lot_gate_close', 'trolley',
+#                'charging_pile', 'reflector', 'distribution_box', 'single_fence', 'multi_fence', 'trash_can',
+#                'parking_sign', 'triangle_warning',
+#                'crash_barrel',
+#                ]
+# ignore_classes = ['nonmotor_vehicle_group', 'fire_hydrant', 'undefined_vehicle', 'undefine_vehicle']
+# class_names = vehicle_classes + person_classes + rider_classes
 
 valid_cameras = ['FISHEYE_CAMERA_BACK', 'FISHEYE_CAMERA_FRONT',
                  'FISHEYE_CAMERA_LEFT', 'FISHEYE_CAMERA_RIGHT',
@@ -30,6 +78,10 @@ camera_ranks = {'FISHEYE_CAMERA_FRONT': 0,
                 'FISHEYE_CAMERA_BACK': 2,
                 'FISHEYE_CAMERA_REAR': 2,
                 'FISHEYE_CAMERA_LEFT': 3}
+
+
+def get_branch_id(class_name):
+    pass
 
 
 def filter_and_reorganize_camera_annos(camera_json_data):
@@ -98,7 +150,8 @@ def convert(lidar_pkl_data, camera_annotations):
                 'lidar2ego_rotation'],
             'ego2global_translation': [0, 0, 0],
             'ego2global_rotation': [1, 0, 0, 0],
-            'camera_ranks': camera_ranks
+            'camera_ranks': camera_ranks,
+            'timestamp': int(lidar_info['timestamp'])
         })
         cams2del = set(lidar_info['cams'].keys()) - set(valid_cameras)
         for invalid_cam in cams2del:
@@ -141,14 +194,14 @@ def convert(lidar_pkl_data, camera_annotations):
                     else:
                         camera_instances[anno['id']] = [anno]
         # get valid instance ids
-        keeps_idx = [idx for idx, id in enumerate(lidar_instance_ids) if id in camera_instances.keys()]
+        keeps_idx = [idx for idx, identity in enumerate(lidar_instance_ids) if identity in camera_instances.keys()]
 
         # fill 3d annotations
         gt_names = lidar_info['gt_names'][keeps_idx]
         lidar_info.update({
             'gt_boxes': lidar_info['gt_boxes'][keeps_idx],
             'gt_names': gt_names,
-            'gt_labels': [class_names.index(label_name) for label_name in gt_names],
+            'gt_labels': [class_names[label_name] for label_name in gt_names],
             'gt_ids': lidar_info['gt_ids'][keeps_idx],
             'num_lidar_pts': lidar_info['num_lidar_pts'][keeps_idx],
             'valid_flag': lidar_info['valid_flag'][keeps_idx],
@@ -162,8 +215,9 @@ def convert(lidar_pkl_data, camera_annotations):
             cam_infos_new[camera_ranks[cam_type]] = cam_info
 
         # fill 2d annotations
-        gt_2d_boxes = np.zeros((len(keeps_idx), len(cam_infos_new), 8))
-        # num_obj, num_cam, (outer_exists_this_cam, inner_exists_this_cam, outer_x, outer_y, outer_w, outer_h, inner_x1, inner_x2)
+        gt_2d_boxes = np.zeros((len(keeps_idx), len(cam_infos_new), 9))
+        # num_obj, num_cam, (outer_exists_this_cam, inner_exists_this_cam, outer_x, outer_y, outer_w, outer_h, inner_x1, inner_x2, inner_type)
+        # inner_type: 0: none, 1: head  2: rear
         gt_wheel_kps = np.zeros((len(keeps_idx), len(cam_infos_new), 4))  # num_obj, num_cam, (x1, y1, x2, y2)
         for row_idx, lidar_idx in enumerate(keeps_idx):
             instance_annos = camera_instances[lidar_instance_ids[lidar_idx]]
@@ -175,7 +229,7 @@ def convert(lidar_pkl_data, camera_annotations):
                         gt_2d_boxes[row_idx][cam_rank][3] = single_cam_anno['bbox'][1]
                         gt_2d_boxes[row_idx][cam_rank][4] = single_cam_anno['bbox'][2]
                         gt_2d_boxes[row_idx][cam_rank][5] = single_cam_anno['bbox'][3]
-                        if single_cam_anno['category_name'] in vehicle_classes:
+                        if class_names[single_cam_anno['category_name']] == 'vehicle':
                             inner_boxes = [box for box in single_cam_anno['stage_two_bbox2d'] if
                                            box['category_name'] in ['head', 'rear']]
                             kps = [point for point in single_cam_anno['stage_two_kps'] if
@@ -186,6 +240,7 @@ def convert(lidar_pkl_data, camera_annotations):
                                 gt_2d_boxes[row_idx][cam_rank][6] = inner_boxes[0]['bbox'][0]
                                 gt_2d_boxes[row_idx][cam_rank][7] = inner_boxes[0]['bbox'][0] + \
                                                                     inner_boxes[0]['bbox'][2]
+                                gt_2d_boxes[row_idx][cam_rank][8] = 1 if inner_boxes[0]['category_name'] == 'head' else 2
                             if len(kps) == 1:
                                 gt_wheel_kps[row_idx][cam_rank][0] = kps[0]['bbox'][0]
                                 gt_wheel_kps[row_idx][cam_rank][1] = kps[0]['bbox'][1]
@@ -199,19 +254,39 @@ def convert(lidar_pkl_data, camera_annotations):
             'gt_2d_boxes': gt_2d_boxes,
             'gt_wheel_kps': gt_wheel_kps
         })
-        lidar_info['anno_infos_2d'] = anno_infos_2d
+        lidar_info['ann_infos'] = anno_infos_2d
+
+
+parser = argparse.ArgumentParser(description='Convert cyl mono parking dataset to bevdet format')
+parser.add_argument(
+    '--pkl_path',
+    help='the dir to load lidar annotations',
+    default='/mnt/storage/parking_dataset/cyl_mono_infos_train_parking.pkl'
+)
+parser.add_argument(
+    '--json_path',
+    help='the dir to load camera annotations',
+    default='/mnt/storage/parking_dataset/cyl_mono_infos_train_parking.coco.json'
+)
+parser.add_argument(
+    '--out_dir',
+    help='the dir to load camera annotations',
+    default='/mnt/storage/parking_dataset/bevdetv3-nuscenes_infos_train.pkl'
+)
+args = parser.parse_args()
 
 if __name__ == '__main__':
     # with open('./data/nuscenes/bevdetv3-nuscenes_infos_train.pkl', 'rb') as f:
     #     bevdet_data = pickle.load(f)
 
-    with open('/mnt/storage/parking_dataset/cyl_mono_infos_train_parking.pkl', 'rb') as f:
+    with open(args.pkl_path, 'rb') as f:
         parking_pkl_data = pickle.load(f)
 
-    with open('/mnt/storage/parking_dataset/cyl_mono_infos_train_parking.coco.json', 'r') as f:
+    with open(args.json_path, 'r') as f:
         parking_json_data = json.load(f)
 
     new_annos = filter_and_reorganize_camera_annos(parking_json_data)
     convert(parking_pkl_data, new_annos)
-    with open('/mnt/storage/parking_dataset/bevdetv3-nuscenes_info_train.pkl', 'wb') as f:
+
+    with open(args.out_dir, 'wb') as f:
         pickle.dump(parking_pkl_data, f)
